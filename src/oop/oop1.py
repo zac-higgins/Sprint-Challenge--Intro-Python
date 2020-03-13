9926fd2396015680c8ea8a0d47ac4d502bb9c01d
Zac Higgins <zachiggins@Higgins-iMac.local>
2020-03-13 09:39:38 -0600

    fixed oop1. completed oop2

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -22,41 +22,35 @@
 
 
 class Vehicle:
-    def __init__(self, name):
-        self.name = name
+    pass
 
-# children of Vehicle
+    # children of Vehicle
 
 
 class FlightVehicle(Vehicle):
-    def __init__(self, name):
-        super().__init__(name)
+    pass
 
 
 class GroundVehicle(Vehicle):
-    def __init__(self, name):
-        super().__init__(name)
+    pass
 
 
 # Children of FlightVehicle
 
 class Starship(FlightVehicle):
-    def __init__(self, name):
-        super().__init__(name)
+    pass
 
 
 class Airplane(FlightVehicle):
-    def __init__(self, name):
-        super().__init__(name)
+    pass
+
 
 # Children of GroundVehicle
 
 
 class Car(GroundVehicle):
-    def __init__(self, name):
-        super().__init__(name)
+    pass
 
 
 class Motorcycle(GroundVehicle):
-    def __init__(self, name):
-        super().__init__(name)
+    pass
--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -3,12 +3,14 @@
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
+
 class GroundVehicle():
-    def __init__(self, num_wheels):
-        self.num_wheels = num_wheels
+    def __init__(self):
+        self.num_wheels = 4
+        self.sound = "vroooom"
 
-    # TODO
-
+    def drive(self):
+        return self.sound
 
 # Subclass Motorcycle from GroundVehicle.
 #
@@ -17,7 +19,13 @@ class GroundVehicle():
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
-# TODO
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__()
+        self.num_wheels = 2
+        self.sound = "BRAAAP!!"
+
 
 vehicles = [
     GroundVehicle(),
@@ -29,4 +37,5 @@ vehicles = [
 
 # Go through the vehicles list and print the result of calling drive() on each.
 
-# TODO
+for vehicle in vehicles:
+    print(vehicle.drive())
