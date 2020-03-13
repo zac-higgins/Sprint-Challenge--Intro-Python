d05c13851327a66ed7da3602b72ac96b4fde099f
Zac Higgins <zachiggins@Higgins-iMac.local>
2020-03-13 09:09:01 -0600

    made vehicle class hierarchy

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,46 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# base class
+
+
+class Vehicle:
+    def __init__(self, name):
+        self.name = name
+
+# children of Vehicle
+
+
+class FlightVehicle(Vehicle):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+class GroundVehicle(Vehicle):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+# Children of FlightVehicle
+
+class Starship(FlightVehicle):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+class Airplane(FlightVehicle):
+    def __init__(self, name):
+        super().__init__(name)
+
+# Children of GroundVehicle
+
+
+class Car(GroundVehicle):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, name):
+        super().__init__(name)
